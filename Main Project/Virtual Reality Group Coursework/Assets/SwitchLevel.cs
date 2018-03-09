f782dd1d57573025f0d739a51765c672739ce8ee
James Tang <jamestang2905@gmail.com>
2018-03-09 16:37:54 +0000

    Added my world
    Trying to add level triggers (Not done)

---
+++ Main Project/Virtual Reality Group Coursework/Assets/SwitchLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel : MonoBehaviour {
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider FPSController)
+    {
+        if (FPSController.gameObject.tag == "Player")
+        {
+            Scene sceneToLoad = SceneManager.GetSceneByName("Hub World");
+            SceneManager.LoadScene(sceneToLoad.name, LoadSceneMode.Additive);
+            SceneManager.MoveGameObjectToScene(FPSController.gameObject, sceneToLoad);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
