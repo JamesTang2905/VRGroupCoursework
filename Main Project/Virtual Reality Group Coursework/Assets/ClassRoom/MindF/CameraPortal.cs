04ef9554f9efafeef88a72f01606457f0a2ef389
James Tang <jamestang2905@gmail.com>
2018-03-12 19:30:01 +0000

    Added mine and  @czar-ec's changes
    @czar-ec's changes:
    Added spooky hallways
    @jamestang2905's changes:
    Added night sky
    Plus a house and a Pyramid
    Fun fact did you know I can add emojis? Like this! :100: wow we are really living in the future!
    
    Co-Authored-By: CzarEc <czar-ec@users.noreply.github.com>

---
+++ Main Project/Virtual Reality Group Coursework/Assets/ClassRoom/MindF/CameraPortal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPortal : MonoBehaviour {
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 playerOffsetFromPortal = otherPortal.position - playerCamera.position;
+
+
+        transform.position = portal.position + playerOffsetFromPortal;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+	}
+}
---
+++ Main Project/Virtual Reality Group Coursework/Assets/ClassRoom/Scripts/Hallway.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hallway : MonoBehaviour {
+
+    public Camera c;
+    public Material cameraMat;
+
+	// Use this for initialization
+	void Start () {
+		if(c.targetTexture != null)
+        {
+            c.targetTexture.Release();
+        }
+        c.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMat.mainTexture = c.targetTexture;
+	}
+}
