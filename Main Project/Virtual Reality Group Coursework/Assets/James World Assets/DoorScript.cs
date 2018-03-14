165447f4f584716f22fd7382a524dd5beb3db598
James Tang <jamestang2905@gmail.com>
2018-03-14 00:32:55 +0000

    Move player pos and trying to make door work
    THE DOOR KEEPS OPENING, GET OUT OF THE WAY SON. DOOR STUCK DOOR STUCK DOOR STUCK DOOR STUCK!

---
+++ Main Project/Virtual Reality Group Coursework/Assets/James World Assets/DoorScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("OpenDoor");
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        anim.enabled = true;
+    }
+
+    void pauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+
+
+}
