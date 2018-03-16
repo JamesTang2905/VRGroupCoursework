7fe8496113fa3f6b6cf697700ad053d8c7189dc6
James Tang <jamestang2905@gmail.com>
2018-03-16 16:00:49 +0000

    Added music and triggers
    Added some cool music when you get outside for the first time
    (Yeah that's really relatable)

---
+++ Main Project/Virtual Reality Group Coursework/Assets/James World Assets/musictrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musictrigger : MonoBehaviour {
+
+    public AudioClip music;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            source.Play();
+        }
+    }
+}
