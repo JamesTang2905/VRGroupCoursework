ad38bffb1ef9f49ab5af3d89dc6cfe0497a2e69e
James Tang <jamestang2905@gmail.com>
2018-03-19 17:53:29 +0000

    Changed files and added new world
    Changed the voulme of the music trigger, added a audio trigger which is different, added a intro world.
    
    Added some random sounds for cool effects.

---
+++ Main Project/Virtual Reality Group Coursework/Assets/Scripts/audiotrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiotrigger : MonoBehaviour {
+
+    public AudioClip music;
+    private AudioSource source;
+    public bool played = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && !played)
+        {
+            source.Play();
+            played = true;
+        }
+    }
+}
