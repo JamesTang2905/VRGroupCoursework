1d99ff87b7ac4f53058a97dbfee096311c7fe982
James Tang <jamestang2905@gmail.com>
2018-03-20 00:41:56 +0000

    Added voice clips + @czar-ec's changes
    Added edgy voice clips, orginally they were going to be changed in audacity instead of unity but I decided to leave it as it is (And let unity change the sound) in case people ever wanted to hear it.

---
+++ Main Project/Virtual Reality Group Coursework/Assets/ClassRoom/EscapeDoor/escapescript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Source of this thing
+ * https://www.youtube.com/watch?v=nfV45mpskRI
+ * 
+ * Should help with myself if I forget
+ * 
+ */
+ 
+public class escapescript : MonoBehaviour
+{
+
+    private Animator animator;
+    bool doorOpened;
+    GameObject trig;
+
+    // Use this for initialization
+    void Start()
+    {
+        //door initially not open
+        doorOpened = false;
+        animator = GetComponent<Animator>();
+        trig = GameObject.Find("Origin/EscapeTrigger");
+    }
+
+    //trigger function
+    void OnTriggerEnter(Collider col)
+    {
+        //check for player
+        if (col.gameObject.tag == "Player" && trig.GetComponent<escapetrigger>().triggered)
+        {
+            doorOpened = true;
+            door("open");
+        }
+    }
+
+    //closing the door when leaving trigger area
+    void OnTriggerExit(Collider col)
+    {
+        if (doorOpened)
+        {
+            doorOpened = false;
+            door("close");
+        }
+    }
+
+    //trigger animation
+    void door(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
---
+++ Main Project/Virtual Reality Group Coursework/Assets/ClassRoom/EscapeDoor/escapetrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escapetrigger : MonoBehaviour {
+
+    public bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        //check for player
+        if (col.gameObject.tag == "Player")
+        {
+            //escape
+            triggered = true;
+        }
+    }
+}
